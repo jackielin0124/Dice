f5635d3728943812287334f3aa8a465a780bac28
unknown <jalin@697-334-Lorien.sf.k12.internal>
2015-09-15 10:33:46 -0700

    added roll funtion

--- Dice.pde
+++ Dice.pde
@@ -5,8 +5,8 @@ void setup()
 }
 
 
-int distY = 40;
-int distX = 40;
+int distY = 50;
+int distX = 50;
 Die bob;
 
 
@@ -20,12 +20,13 @@ void draw()
 		{
 			bob = new Die(x, y);
 			bob.show();
+			bob.roll();
 		}
 	}
 
 		
 }
-
+ 
 void mousePressed()
 {
 	redraw();
@@ -34,23 +35,55 @@ void mousePressed()
 class Die //models one single dice cube
 {
 	//variable declarations here
-	int toss, myX, myY;
+	int myX, myY, value;
 	Die(int x, int y) //constructor
 	{
 		//variable initializations here
 		myY=y;
 		myX=x;
-
+		value = (int)(Math.random()*6 +1);
 
 	}
 	void roll()
 	{
-		//your code here
+		if (value == 1){
+			ellipse(myX+20, myY+20, 5, 5);
+		}
+		if (value == 2){
+			ellipse(myX+10, myY+10, 5, 5);
+			ellipse(myX+30, myY+30, 5, 5);
+		} 
+		if (value == 3){
+			ellipse(myX+10, myY+10, 5, 5);
+			ellipse(myX+20, myY+20, 5, 5);
+			ellipse(myX+30, myY+30, 5, 5);
+		}
+		if (value == 4){
+			ellipse(myX+10, myY+10, 5, 5);
+			ellipse(myX+30, myY+30, 5, 5);
+			ellipse(myX+10, myY+30, 5, 5);
+			ellipse(myX+30, myY+10, 5, 5);
+		} 
+		if (value == 5){
+			ellipse(myX+10, myY+10, 5, 5);
+			ellipse(myX+30, myY+30, 5, 5);
+			ellipse(myX+20, myY+20, 5, 5);
+			ellipse(myX+10, myY+30, 5, 5);
+			ellipse(myX+30, myY+10, 5, 5);
+		 }
+		 if (value == 6){
+			ellipse(myX+10, myY+10, 5, 5);
+			ellipse(myX+30, myY+30, 5, 5);
+			ellipse(myX+10, myY+20, 5, 5);
+			ellipse(myX+30, myY+20, 5, 5);
+			ellipse(myX+10, myY+30, 5, 5);
+			ellipse(myX+30, myY+10, 5, 5);
+		}
 	}
 	void show()
 	{
 		//your code here
-		rect(myX,myY,30,30,3);
+		rect(myX,myY,40,40,3);
 		
 	}
 }
--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -21,8 +21,8 @@ public void setup()
 }
 
 
-int distY = 40;
-int distX = 40;
+int distY = 50;
+int distX = 50;
 Die bob;
 
 
@@ -36,12 +36,13 @@ public void draw()
 		{
 			bob = new Die(x, y);
 			bob.show();
+			bob.roll();
 		}
 	}
 
 		
 }
-
+ 
 public void mousePressed()
 {
 	redraw();
@@ -50,23 +51,56 @@ public void mousePressed()
 class Die //models one single dice cube
 {
 	//variable declarations here
-	int toss, myX, myY;
+	int myX, myY, value;
 	Die(int x, int y) //constructor
 	{
 		//variable initializations here
 		myY=y;
 		myX=x;
-
+		value = (int)(Math.random()*6 +1);
 
 	}
 	public void roll()
 	{
-		//your code here
+		if (value == 1){
+			ellipse(myX+20, myY+20, 5, 5);
+		}
+		if (value == 2){
+			ellipse(myX+10, myY+10, 5, 5);
+			ellipse(myX+30, myY+30, 5, 5);
+		} 
+		if (value == 3){
+			ellipse(myX+10, myY+10, 5, 5);
+			ellipse(myX+20, myY+20, 5, 5);
+			ellipse(myX+30, myY+30, 5, 5);
+		}
+		if (value == 4){
+			ellipse(myX+10, myY+10, 5, 5);
+			ellipse(myX+30, myY+30, 5, 5);
+			ellipse(myX+10, myY+30, 5, 5);
+			ellipse(myX+30, myY+10, 5, 5);
+		} 
+		if (value == 5){
+			ellipse(myX+10, myY+10, 5, 5);
+			ellipse(myX+30, myY+30, 5, 5);
+			ellipse(myX+20, myY+20, 5, 5);
+			ellipse(myX+10, myY+30, 5, 5);
+			ellipse(myX+30, myY+10, 5, 5);
+		 }
+		 if (value == 6){
+			ellipse(myX+10, myY+10, 5, 5);
+			ellipse(myX+30, myY+30, 5, 5);
+			ellipse(myX+10, myY+20, 5, 5);
+			ellipse(myX+30, myY+20, 5, 5);
+			ellipse(myX+10, myY+30, 5, 5);
+			ellipse(myX+30, myY+10, 5, 5);
+		}
 	}
 	public void show()
 	{
 		//your code here
-		rect(myX,myY,30,30,3);
+		rect(myX,myY,40,40,3);
+		
 	}
 }
 
